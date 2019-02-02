e5709fa805f35d91c2726bf9e5b97167111e82e7
imran-niyaz <46766310+imran-niyaz@users.noreply.github.com>
2019-02-01 22:20:51 -0500

    update to String title

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -32,7 +32,8 @@ public class LoginTest {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        Assert.assertEquals(driver.getTitle(),"Web Orders");
+        String title = driver.getTitle();
+        Assert.assertEquals(title,"Web Orders");
 
     }
 
@@ -45,7 +46,9 @@ public class LoginTest {
         Assert.assertEquals(driver.getTitle(),"Web Orders");
 
         driver.findElement(By.id("ctl00_logout")).click();
-        Assert.assertEquals(driver.getTitle(),"Web Orders Login");
+        String title = driver.getTitle();
+        Assert.assertEquals(title,"Web Orders Login");
+       
 
     }
 
